5b436cca992489e5bd8d8b5cd1ea8d7cef4066df
kabweza <92577963+kabweza@users.noreply.github.com>
2023-08-30 16:02:46 +0300

    More Updates in Path in Gallery, Videos Single

--- resources/views/news/pages/single/gallery.blade.php
+++ resources/views/news/pages/single/gallery.blade.php
@@ -1,11 +1,23 @@
 @extends('news.layouts.app')
 
-@if ($lang == 'en') @section('title') {{ $singleAlbum->album_title }} @endsection @else @section('title') ग्यालेरी @endsection @endif
+@if ($lang == 'en') @section('title')
+    {{ $singleAlbum->album_title }}
+@endsection
+@else
+@section('title')  ग्यालेरी @endsection 
 
+@endif
 @section('content')
     <div class="container">
+
         <div class="page-breadcrumbs">
-            <h1 class="section-title">{{ $singleAlbum->album_title }}</h1>
+            @if ($lang == 'en')
+                <h1 class="section-title" style="font-size: 15px"><a href="{{ route('home') }}"> Home </a> > <a
+                        href="{{ route('gallery') }}"> Gallery</a> > {{ $singleAlbum->album_title }}</h1>
+            @else
+                <h1 class="section-title" style="font-size: 15px"><a href="{{ route('home') }}"> घर </a> > <a
+                        href="{{ route('gallery') }}"> ग्यालेरी </a> > {{ $singleAlbum->album_title }}</h1>
+            @endif
         </div>
         <div class="section">
             <div class="row">
@@ -20,9 +32,9 @@
                                                 <div class="post medium-post">
                                                     <div class="entry-header">
                                                         <div class="entry-thumbnail">
-                                                                <img class="img-fluid"
+                                                            <img class="img-fluid"
                                                                 style="width: 100%; height: 100%; object-fit:cover;"
-                                                                src="{{ "$pF/storage/uploads/gallery/images/".$img->image }}"
+                                                                src="{{ "$pF/storage/uploads/gallery/images/" . $img->image }}"
                                                                 alt="Image" />
                                                         </div>
                                                     </div>
--- resources/views/news/pages/single/video.blade.php
+++ resources/views/news/pages/single/video.blade.php
@@ -5,6 +5,13 @@
 @section('content')
 
 <div class="container">
+    <div class="page-breadcrumbs">
+        @if ($lang == 'en')
+        <h1 class="section-title" style="font-size: 15px"><a href="{{ route('home') }}"> Home </a> > <a href="{{ route('videos') }}"> Videos</a> > {{ $singleVideo->title }}</h1>
+        @else
+        <h1 class="section-title" style="font-size: 15px"><a href="{{ route('home') }}"> घर </a> > <a href="{{ route('videos') }}"> भिडियोहरू </a> > {{ $singleVideo->title }}</h1>
+        @endif
+     </div>
     <div class="row">
         <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-xm-12">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xm-12">
